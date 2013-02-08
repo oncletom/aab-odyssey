276df25feae6720ddfbcbbe59f357cd581ec88bf
oncletom <thomas@oncle-tom.net>
2013-02-08 17:56:12 +0100

    Better testing init, testing fromJSON (failing)

--- tests/unit/lib-channel.js
+++ tests/unit/lib-channel.js
@@ -1,12 +1,30 @@
 "use strict";
 
 suite('Channel Library', function(){
+  var accepted_keys = ['id', 'name', 'sort_order', 'image'];
+
   test('constructor', function(){
-    expect(new Channel()).to.only.have.keys('id', 'name', 'sort_order', 'image');
+    expect(function(){ new Channel() }).not.to.throwException();
+    expect(new Channel()).to.only.have.keys(accepted_keys);
   });
 
-  test.skip('#fromJSON()', function(){
+  test('#fromJSON()', function(){
+    var item = new Channel();
+    var data = [
+      {},
+      { "id": "aab_one", "name": "AAB One", "image": "/dev/null", "sort_order": 2001 }
+    ];
 
+    // Checking we have the proper values for each objects
+    data.forEach(function(json){
+      item = Channel.fromJSON(json);
+
+      expect(item).to.only.have.keys(accepted_keys);
+
+      accepted_keys.forEach(function(key){
+        expect(item[key]).to.be(json[key]);
+      });
+    });
   });
 
   test.skip('#getList()', function(){
