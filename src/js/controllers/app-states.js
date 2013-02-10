e5664d80be0ab2fabd3bb4b5f6ef2f171ad415c9
oncletom <thomas@oncle-tom.net>
2013-02-10 06:12:53 +0100

    State observer

--- src/js/controllers/app-states.js
+++ src/js/controllers/app-states.js
@@ -12,16 +12,32 @@ function AppStatesController($){
    */
   (function init(){
     self.setState('init');
+
+    $('[data-state-controller]').forEach(function(el){
+      self.observe(el);
+    });
   })();
 }
+
+AppStatesController.prototype.observe = function observe(el, event){
+  var container = el;
+  var self = this;
+
+  $(el).on(event || 'click', 'a', function observer(e){
+    self.setState(el.getAttribute('data-state-controller'), {
+      hash: e.currentTarget.hash.replace(/^#/, '')
+    });
+  });
+};
 
 /**
  * Triggers a new App State
  *
  * @event state:set {Array.<String, String>}}
  */
-AppStatesController.prototype.setState = function setState(new_state){
+AppStatesController.prototype.setState = function setState(new_state, args){
   this.broadcaster.trigger('state:set', [new_state, this.state]);
+  this.broadcaster.trigger('state:is:' + new_state, [args]);
 
   $('[data-state-visible]').forEach(function changeState(el){
     var $el = $(el);
